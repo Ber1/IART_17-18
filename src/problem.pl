b011e641cf07187c3958b85e81b07d388d04591e
r_tor <r_torres96@live.com.pt>
2018-04-08 20:37:21 +0100

    minor change

--- src/problem.pl
+++ src/problem.pl
@@ -1,7 +1,7 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
 
 %locais onde existem pessoas para evacuar
-local_inicial(0,0,20).
+local_inicial(local(0,0,20)).
 %estado inicial
 %local(Coordenada X, Coordenada Y, número de pessoas)
 estado_inicial(local(10,0,0)).
@@ -24,13 +24,13 @@ ponto_estrategico(ponto(6,0)).
 %sucessor pontos     
 sucessor(local(X,Y,N), local(X,Y,NN),ponto(PX,PY),C,LP) :-
         \+ member(ponto(PX,PY),LP),
-        local_inicial(LX,LY,_),
+        local_inicial(local(LX,LY,_)),
         C is (abs(PX-LX)+abs(PY-LY)),
         capacidade_veiculos(Capacidade),
         NN is N+Capacidade.
 
 sucessor_ponto_final(local(X,Y,N), local(X,Y,NN),C) :-
-        local_inicial(LX,LY,_),
+        local_inicial(local(LX,LY,_)),
         C is (abs(X-LX)+abs(Y-LY)),
         capacidade_veiculos(Capacidade),
         NN is N+Capacidade.
