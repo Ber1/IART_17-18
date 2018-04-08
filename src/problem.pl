ea967e4896ea742fae3ffdfc89c3deb5fb57829b
r_tor <r_torres96@live.com.pt>
2018-04-08 16:20:42 +0100

    v1.0 done

---
+++ src/problem.pl
@@ -0,0 +1,44 @@
+/* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
+
+%locais onde existem pessoas para evacuar
+local_inicial(0,0,20).
+%estado inicial
+%local(Coordenada X, Coordenada Y, número de pessoas)
+estado_inicial(local(10,0,0)).
+
+%estado final
+%local(Coordenada X, Coordenada Y, número de pessoas)
+estado_final(local(10,0,20)).
+
+capacidade_veiculos(5).
+
+%ponto estratégicos
+%local(Coordenada X, Coordenada Y, número de pessoas que podem ser evacuadas numa viagem)
+ponto_estrategico(ponto(1,0)).
+ponto_estrategico(ponto(2,0)).
+ponto_estrategico(ponto(3,0)).
+ponto_estrategico(ponto(4,0)).
+ponto_estrategico(ponto(5,0)).
+ponto_estrategico(ponto(6,0)).
+
+%sucessor pontos     
+sucessor(local(X,Y,N), local(X,Y,NN),ponto(PX,PY),C,LP) :-
+        \+ member(ponto(PX,PY),LP),
+        local_inicial(LX,LY,_),
+        C is (abs(PX-LX)+abs(PY-LY)),
+        capacidade_veiculos(Capacidade),
+        NN is N+Capacidade.
+
+sucessor_ponto_final(local(X,Y,N), local(X,Y,NN),C) :-
+        local_inicial(LX,LY,_),
+        C is (abs(X-LX)+abs(Y-LY)),
+        capacidade_veiculos(Capacidade),
+        NN is N+Capacidade.
+
+
+
+        
+        
+        
+     
+
