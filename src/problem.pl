adb6d6b597d58ee57b078679e9755ef92d1b1099
r_tor <r_torres96@live.com.pt>
2018-05-20 03:04:27 +0100

    maybe a basic interface tomorrow.

--- src/problem.pl
+++ src/problem.pl
@@ -1,56 +1,22 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
 
 %locais onde existem pessoas para evacuar
-local_inicial(local(0,0,6)).
-local_inicial(local(1,0,6)).
-local_inicial(local(12,0,6)).
-local_final(10,0).
-
-:- dynamic estado_inicial/1.
-:- dynamic estado_final/1.
+local_inicial(local(0,1,12)).
+local_inicial(local(1,2,17)).
+local_final(10,1).
 
 capacidade_veiculos(5).
 
 %ponto estratégicos
 %local(Coordenada X, Coordenada Y, número de pessoas na carrinha)
-ponto_estrategico(ponto(2,0,0)).
-ponto_estrategico(ponto(3,0,0)).
-ponto_estrategico(ponto(4,0,0)).
-ponto_estrategico(ponto(5,0,0)).
-ponto_estrategico(ponto(6,0,0)).
-ponto_estrategico(ponto(7,0,0)).
+ponto_estrategico(ponto(2,1,0)).
+ponto_estrategico(ponto(3,2,0)).
+ponto_estrategico(ponto(4,3,0)).
+ponto_estrategico(ponto(5,4,0)).
+ponto_estrategico(ponto(6,1,0)).
+ponto_estrategico(ponto(7,2,0)).
 
 
-
-sucessor(Locais,Pontos, NewE,local(XL,YL,NP),ponto(XP,YP,PP),PosLocal,PosPonto,C):-
-        NP>0,
-        capacidade_veiculos(CV),
-        Capacity is CV-PP,
-        NP>=Capacity,
-        NewNP is NP-Capacity,
-        replace(PosLocal,Locais,local(XL,YL,NewNP),NewLocais),
-        local_final(LFX,LFY),
-        replace(PosPonto,Pontos,ponto(LFX,LFY,0),NewPontos),
-        NewE = [NewLocais,NewPontos],
-        C is abs(XL-XP)+abs(YL-YP).
-
-sucessor(Locais,Pontos, NewE,local(XL,YL,NP),ponto(XP,YP,PP),PosLocal,PosPonto,C):-
-        NP>0,
-        capacidade_veiculos(CV),
-        Capacity is CV-PP,
-        NP<Capacity,
-        NewPP is PP+NP,
-        replace(PosLocal,Locais,local(XL,YL,0),NewLocais),
-        replace(PosPonto,Pontos,ponto(XL,YL,NewPP),NewPontos),
-        NewE = [NewLocais,NewPontos],
-        C is abs(XL-XP)+abs(YL-YP).
-
-sucessor(Locais,Pontos, NewE,_,ponto(_,_,PP),_,PosPonto,C):-
-        PP>0,
-        local_final(LFX,LFY),
-        replace(PosPonto,Pontos,ponto(LFX,LFY,0),NewPontos),
-        NewE = [Locais,NewPontos],
-        C is 0.
         
 
         
--- src/solver.pl
+++ src/solver.pl
@@ -1,18 +1,17 @@
 /* -*- Mode:Prolog; coding:iso-8859-1; indent-tabs-mode:nil; prolog-indent-width:8; prolog-paren-indent:4; tab-width:8; -*- */
 :- reconsult(algorithms).
+:- reconsult(problem).
+
+:- dynamic estado_inicial/1.
 
 
 solve_problem:-
-        retractall(estado_final(_)),
         retractall(estado_inicial(_)),
         findall(LocalX,local_inicial(LocalX),Locais),
         findall(PontoX,ponto_estrategico(PontoX),Pontos),
         assert(estado_inicial([Locais, Pontos])),
-        findall(LocalX, (local_inicial(local(X,Y,_)),LocalX= local(X,Y,0)),Locais_final),
-        findall(PontoX, (ponto_estrategico(_), PontoX= ponto(_,_,0)),Pontos_final),
-        assert(estado_final([Locais_final, Pontos_final])),
         solve_astar(Sol),
         print_sol(Sol).
        
 print_sol([]).        
-print_sol([H|T]):-print_sol(T),write(H),nl.
+print_sol([H|T]):-write(H),nl,print_sol(T).
